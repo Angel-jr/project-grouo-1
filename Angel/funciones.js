26b840b435e5405f04c922d9661d9ef53c080f8c
Angel-jr <angelfp28@gmail.com>
2023-03-08 20:53:47 -0300

    Angel2

---
+++ Angel/funciones.js
@@ -0,0 +1,15 @@
+const suma = (x,y) =>{
+    return 20 + 10;
+}
+
+const multiplica = (x,y) => {
+    return 5 * 5;
+}
+
+const resta = (x,y) => {
+    return 25 - 5; 
+}
+
+const dividir = (x,y) =>{
+    return 25 / 2;
+}
